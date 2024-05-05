b26add6ed1f97811e0c835ad98782d6cc6750a0e
Josseyo Yoshida <josefin@yomaco.com>
2024-05-05 19:03:49 +0000

    Game setup

--- run.py
+++ run.py
@@ -1,3 +1,26 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+import os
+os.system ("clear")
+
+# Initialize
+word = "YOMACO"
+guess = "------"
+wrong_letters = ""
+
+#Print header
+print("HANGMAN/n")
+
+#Main game loop
+while True:
+    print(f"Current Guess: {guess}")
+
+    letter = input("Please enter a letter: ").upper()
+
+    # Check if the letter is in the word
+    if letter in word:
+        print("GOOD GUESS!")
+    else:
+        print("BAD GUESS")
