875ada836d49a9f88c9ba3691b952fa30ce53524
Josseyo Yoshida <josefin@yomaco.com>
2024-05-15 06:55:18 +0000

    Fix indentations in def gameplay(),
    Add blank rows between defs,
    
    Delete first while True: in def gameplay
    
    Add function  play_again
    
    Add all functions to def main

--- run.py
+++ run.py
@@ -1,14 +1,16 @@
 import random
 import os
-from words import words
-from men import menu
+from words import categories
+from men import men
+
 
 def random_word():
     category = random.choice(list(categories.keys()))
     word = random.choice(categories[category])
     return category.upper(), word.upper()
 
-def generate_blanks(word): 
+
+def generate_blanks(word):
     """
     Generates a string of blanks based on the length of the word.
     """
@@ -18,11 +20,12 @@ def generate_blanks(word):
             blanks += "_"
     return blanks
 
+
 def get_guess(used_letters):
-     """
+    """
     Prompts the user to guess a letter and validates the input.
     """
-     while True:
+    while True:
         guess = input("guess: ")
         if len(guess) != 1:
             print("guess must be one character")
@@ -41,54 +44,75 @@ def reveal_letters(blanks, word, guess):
     for i, char in enumerate(word):
         if char == guess:
             blanks[i] = guess
-    return "".join(blanks) 
+    return "".join(blanks)
+
 
 def draw_guy(lives):
-    """ 
+    """
     Prints the hangman diagram based on the number of remaining lives.
     """
-    print(f"lives remaining: {lives}")
+    return men()[6 - lives]
 
-def main():
-    while True:
+
+def display_status(category, blanks, lives, used_letters):
+    os.system("clear")
+    print(category)
+    print(blanks)
+    print(f"{lives=}")
+    print(f"{used_letters}")
+    print(draw_guy(lives))
+
+
+def gameplay():
         category, word = random_word()
         blanks = generate_blanks(word)
         lives = 6
         used_letters = ""
 
         while True:
-            os.system("clear")
-            print(category)
-            print(blanks)
-            print(f"{lives=}")
-            print(f"{used_letters=}")
-            draw_guy(lives)
+            display_status(category, blanks, lives, used_letters)
 
-            guess = get_guess(used_letters, word)
+            guess = get_guess(used_letters)
+
             if guess == "SOLVED":
+                # function for game over summary
+                display_status(category, blanks, lives, used_letters)
+                print("You win")
                 break
-                #function for game over summary
-    
-            print(word)
-            print("You win")
-            break
 
             if guess in word:
                 blanks = reveal_letters(blanks, word, guess)
-        else:
-            lives -= 1
-            print(draw_guy(lives))
-            used_letters += guess
+            else:
+                lives -= 1
+                used_letters += guess
 
-        if "_" not in blanks:
-            print(f"The word was: {word}")
-            print("you win")
-            break
+            if "_" not in blanks:
+                display_status(category, blanks, lives, used_letters)
+                print("You win")
+                break
 
-        if lives < 1:
-            print(f"The word was: {word}")
-            print("You lose")
-            break
+            if lives < 1:
+                blanks = word
+                display_status(category, blanks, lives, used_letters)
+                print("You lose")
+                break
+    
+def play_again():
+    start_over = input('hit y to play again or any other key to exit: ')
+    if start_over.lower() == "y":
+        main()
+    else:
+        exit()
 
-if __name__ == "__main__":
-    main()
+
+def main():
+    category, word = random_word()
+    blanks = generate_blanks(word)
+    lives = 6
+    used_letters = ""
+    display_status(category, blanks, lives, used_letters)
+    gameplay()
+    play_again()
+
+
+main()
